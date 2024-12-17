2ea2f8034cddc3f5650b0d47ba2e17c58dc85ce0
Patrick Little <little.patrick@outlook.com>
2024-12-17 15:54:15 -0700

    iteration 3 complete

--- lib/store_facilities.rb
+++ lib/store_facilities.rb
@@ -9,10 +9,27 @@ class StoreFacilities
 
   def create_facility(store)
     @facility = store.map do |store_data|
-      Facility.new({
-        name: = 
-        address: =
-        phone: = 
-      })
-
+      if store_data.include?(:dmv_office) #Co
+        Facility.new({
+          name: store_data[:dmv_office],
+          address: "#{store_data[:address_li]}, #{store_data[:address__1]}, #{store_data[:city]}, #{store_data[:state]}, #{store_data[:zip]}",
+          phone: store_data[:phone]
+        })
+      elsif store_data.include?(:office_name) #NY
+        Facility.new({
+          name: store_data[:office_name],
+          address: "#{store_data[:street_address_line_1]}, #{store_data[:city]}, #{store_data[:state]}, #{store_data[:zip_code]}",
+          phone: "#{store_data[:":@computed_region_yamh_8v7k"]}-#{store_data[:":@computed_region_wbg7_3whc"]}-#{store_data[:":@computed_region_kjdx_g34t"]}"
+        })
+      elsif store_data.include?(:agent) #Mo
+        Facility.new({
+          name: store_data[:name],
+          address: "#{store_data[:address1]}, #{store_data[:city]}, #{store_data[:state]}, #{store_data[:zipcode]}",
+          phone: store_data[:phone]
+        })
+      else
+        print "Unable to read data"
+      end
+    end
+  end
 end
